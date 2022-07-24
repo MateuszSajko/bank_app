f180a82f62ad9c50b3f494f7365474c96a511655
MateuszSajko <mateuszsajko@gmail.com>
2022-07-24 14:16:24 +0200

    Dodanie opcji - wplata, wyplata gotowki z informacja o stanie konta w klasie AccountDetails

--- src/main/java/AccountDetails.java
+++ src/main/java/AccountDetails.java
@@ -16,11 +16,24 @@ public class AccountDetails {
         System.out.println("Provide your account balance");
         balance = scanner.nextLong();
     }
-
     public void showAccount (){
         System.out.println("Your account name is " + name);
         System.out.println("Your account number is " + accountNumber);
         System.out.println("Your account balance is " + balance);
+
+    }
+    public void deposit (){
+        System.out.println("Enter ammount of money you want to add to your account ");
+        long cashAdd = scanner.nextLong();
+        balance = cashAdd + balance;
+        System.out.println("Yours current sallary is " + balance +  "\n");
+    }
+
+    public void withdrawal() {
+        System.out.println("Enter the amount of money you want to withdraw from your account");
+        long cashOut = scanner.nextLong();
+        balance = balance - cashOut;
+        System.out.println("Yours current sallary is " + balance +  "\n");
 
     }
 }
